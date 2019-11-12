1ebce512bc5fc275af73cc66ad9957fc6b28e87b
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-12 11:34:10 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,11 @@
-def square_array(array)
-  new_array = []
-  array.each do |element|
-    new_array << element ** 2
-  end 
+# def square_array(array)
+#   new_array = []
+#   array.each do |element|
+#     new_array << element ** 2
+#   end 
 
-    return new_array
-end 
+#     return new_array
+# end 
 
 
 def square_array(array)
